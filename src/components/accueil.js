d7314cb70cb19c3b57e9ab384cd63a66c363145d
Antto <114882531+Antto0509@users.noreply.github.com>
2024-05-06 20:33:48 +0200

    Update

---
+++ src/components/accueil.js
@@ -0,0 +1,16 @@
+function accueil() {
+    return (
+        <div>
+            <h1>Bienvenue</h1>
+            <p>Vous trouverez ci-dessous les différentes fonctionnalités de l'application</p>
+            <ul>
+                <li><a href="#">Étudiants</a></li>
+                <li><a href="#">Enseignants</a></li>
+                <li><a href="#">Matiere</a></li>
+                <li><a href="#">Notes</a></li>
+            </ul>
+        </div>
+    );
+}
+
+export default accueil;
