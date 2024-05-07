f2e7cc9ca81dea74ac124273c066ecbf8363a327
Antto <114882531+Antto0509@users.noreply.github.com>
2024-05-07 14:01:18 +0200

    Update

--- src/server/server.js
+++ src/server/server.js
@@ -5,3 +5,25 @@ app.get('/', (req, res) => { res.send('Hello server ...');
 });
 app.listen(port, () => {
 console.log(`Server is running on port ${port}`) })
+
+// Connexion à la base de données
+const mongoose = require('mongoose');
+const uriCompass = "mongodb://127.0.0.1:27017/mon-universiteDB"
+mongoose.connect(uriCompass).then(()=> console.log("successful connexion DB"));
+
+// Création du schéma
+const Schema = mongoose.Schema; let EtudSchema = new Schema({
+    code: Number,
+    nom: String, }, 
+    { versionKey: false }
+);
+
+// Création du modèle
+let Etud = mongoose.model("Etudiants ", EtudSchema);
+
+// Affichage de tous les étudiants
+app.get("/Etudiants", (req, res) => {
+    Etud.find({}, (err, results) => {
+        if (!err) { res.send(results);}
+    });
+});
